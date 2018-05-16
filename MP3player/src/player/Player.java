6114d2fb6428b18890575210fa0e0acb943a0eec
ParadoxZW <964730078@qq.com>
2018-05-16 19:29:55 +0800

    build gui in eclipse project

---
+++ MP3player/src/player/Player.java
@@ -0,0 +1,50 @@
+package player;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
+public class Player implements Runnable{
+    Thread t;
+    private String dir;
+    private javazoom.jl.player.Player player;
+    public Player(String dir){   //创建MP3Play的对象，并传入歌曲本地路径
+        this.dir = dir;
+    }
+    public void setDir(String dir){
+        this.dir = dir;
+    }
+    private void play(){     //调用play()方法就可以播放了
+        try{
+            BufferedInputStream buffer = new BufferedInputStream(new FileInputStream(dir));
+            player = new javazoom.jl.player.Player(buffer);
+            player.play();
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public void stop(){     //调用stop()方法可以让歌曲停止播放
+        player.close();
+    }
+    private int getPosition(){
+        return player.getPosition();
+    }
+    @Override
+    public void run(){      //用线程启动play()，这样才能正常使用stop()
+        play();
+    }
+    public void start(){
+        t = new Thread(this);
+        t.start();
+    }
+
+//    public static void main(String[] args){
+//        Player mp3 = new Player("C:\\Users\\牟宇\\Music\\赵雷 - 成都.mp3"); //创建MP3Play的对象，并传入歌曲本地路径
+//        mp3.start(); //调用play()方法就可以播放了
+//        try {
+//            Thread.currentThread().sleep(5000);  //让线程睡眠5秒
+////            mp3.stop();     //测试是否能让歌曲停止播放
+//            System.out.println(mp3.getPosition());      //获取当前歌曲从播放开始的毫秒数
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
---
+++ MP3player/src/song/Song.java
@@ -0,0 +1,47 @@
+package song;
+public class Song {
+    private String id; // 歌曲编号 考虑到我们可能会下载两首同名歌曲 所以歌曲统一以 编号.mp3 命名格式保存在本地
+    //我们会在本地建一个txt文件记录已经下载的歌曲的id和歌名、歌手、文件路径等用于本地检索的信息
+    private String name;
+    private String artist_name;
+    private String address; //  歌曲网上链接
+    private String dir; //  歌曲文件本地路径
+    private String lyrics; // 备用功能
+    // 预留专辑封面.jpg属性
+    void setId(String id){
+        this.id = id;
+    }
+    void setName(String name){
+        this.name = name;
+    }
+    void setArtist_name(String artist_name){
+        this.artist_name = artist_name;
+    }
+    void setAddress(String address){
+        this.address =address;
+    }
+    void setDir(String dir){
+        this.dir = dir;
+    }
+    void setLyrics(String lyrics){
+        this.lyrics = lyrics;
+    }
+    String getId(){
+        return id;
+    }
+    String getName(){
+        return name;
+    }
+    String getArtist_name(){
+        return artist_name;
+    }
+    String getAddress(){
+        return address;
+    }
+    String getDir(){
+        return dir;
+    }
+    String getLyrics(){
+        return lyrics;
+    }
+}
