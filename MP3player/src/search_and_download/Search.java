e712a5d3c029cfff70fbf28818a82faf9d4b4f2b
ParadoxZW <964730078@qq.com>
2018-05-17 20:32:16 +0800

    some code for search

--- MP3player/src/search_and_download/Search.java
+++ MP3player/src/search_and_download/Search.java
@@ -1,4 +1,10 @@
 package search_and_download;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import song.Song;
 
@@ -8,16 +14,30 @@ public class Search {
 		 * 输入歌名，返回歌曲数组 */
 		ArrayList<Song> list;
 		list = new ArrayList<Song>();
-		StringBuilder response = new StringBuilder();
-		get(name, response);
-		System.out.println(response.hashCode());
-		System.out.println(response.toString());
+		new Thread(new Runnable() {  
+            @Override  
+            public void run() {  
+                try {  
+                    HttpURLConnection connection;  
+                    String finalTitle = URLEncoder.encode(name,"utf-8");  
+                    URL url = new URL("http://tingapi.ting.baidu.com/v1/restserver/ting?from=webapp_music&method=baidu.ting.search.catalogSug&format=json&callback=&query="+name);  
+                    connection = (HttpURLConnection) url.openConnection();  
+                    connection.setConnectTimeout(60*1000);  
+                    connection.setReadTimeout(60*1000);  
+                    connection.connect();  
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));  
+                    String s;  
+                    if ((s=reader.readLine())!=null)  
+//                        doJson(s);  
+                    	System.out.println(s);
+                } catch (IOException e) {  
+                    e.printStackTrace();  
+                }  
+            }  
+        }).start();  
 		return list;
 	}
-	private static void get(String name, StringBuilder response) {
-		GetRun getRun = new GetRun(response, "http://tingapi.ting.baidu.com/v1/restserver/ting?from=webapp_music&method=baidu.ting.search.catalogSug&format=json&callback=&query="+name);
-		getRun.start();	
-	}
+	
 	public static void main(String[] args) {
 		search("告白气球");
 		System.out.println("1");
